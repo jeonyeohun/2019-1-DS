7db4d3a1201dea195a1912754848a4986bc2e40f
jeonyeohun <jeon.yeohun@gmail.com>
2019-03-18 20:44:25 +0900

    가규먼트, 에러처리

--- include/sort.h
+++ include/sort.h
@@ -11,13 +11,13 @@
 #include <sstream>
 #include <string>
 #include <cstring>
-
-void insertionSort(int *list, int n, int(*comp)(int a, int b));
-void selectionSort(int *list, int n, int(*comp)(int a, int b));
-void quickSort(int *list, int n, int(*comp)(int a, int b));
-void bubbleSort(int *list, int n, int(*comp)(int a, int b));
 int ascending (int a, int b) {return a - b;}
 int descending (int a, int b) {return b - a;}
+void insertionSort(int *list, int n, int(*comp)(int a, int b)=ascending);
+void selectionSort(int *list, int n, int(*comp)(int a, int b)=ascending);
+void quickSort(int *list, int n, int(*comp)(int a, int b)=ascending);
+void bubbleSort(int *list, int n, int(*comp)(int a, int b)=ascending);
+
 
 
 #endif
--- psets/pset-extra1/sort.cpp
+++ psets/pset-extra1/sort.cpp
@@ -8,11 +8,15 @@
 #include <cmath>
 #include <cassert>
 #include <ctime>
+#include <ctype.h>
+#include <stdlib.h>
 #include "nowic.h"
 #include "sort.h"
 using namespace std;
 
-
+int isStringInt(string str){
+		return atoi(str.c_str());
+}
 
 void print_list(int *list, int n) {
 	for (int i = 0; i < n; i++)
@@ -21,11 +25,26 @@ void print_list(int *list, int n) {
 }
 
 #if 1
-int main() {
+int main(int argc, char* argv[]) {
 	int N = 0;
 	string name[4] = {"BUBBLE", "INSERT", "QUICK", "SELECT"};
 	clock_t start, end;
-	N = GetInt();
+
+	if (argc == 1){
+		cout << "Enter a number of samples: ";
+		cin >> N;
+	}
+	else{
+		N = isStringInt(argv[1]);
+	}
+
+	if (N==0 || N<0){
+		cout << "Your input (" << N << ") cannot be proccessed" << endl;
+		cout << "Usage: sort[N]" << endl;
+		exit(1);
+	}
+
+
 	void (*fp[]) (int*, int, int(*comp)(int, int)) = {bubbleSort, insertionSort, quickSort, selectionSort};
 
 	// int *list = (int *)malloc(N * sizeof(int));
@@ -41,7 +60,7 @@ int main() {
 	cout << "NOTsorted: ";
 	print_list(list, N);
 
-	selectionSort(list, N, ascending);
+	quickSort(list, N);
 	cout << "sorted: ";
 	print_list(list, N);
 
@@ -51,7 +70,7 @@ int main() {
 	cout << "NOTsorted: ";
 	print_list(list, N);
 
-	selectionSort(list, N, ascending);
+	quickSort(list, N, ascending);
 	cout << "sorted: ";
 	print_list(list, N);
 
@@ -61,7 +80,7 @@ int main() {
 	cout << "NOTsorted: ";
 	print_list(list, N);
 
-	selectionSort(list, N, descending);
+	quickSort(list, N, descending);
 
 	cout << "sorted: ";
 	print_list(list, N);
